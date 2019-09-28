f31f9d24e60dcc901cd4addbae9879e4e6693f81
dora1405 <li.dora05@gmail.com>
2019-09-28 14:45:24 -0700

    modified booking_manager.rb to showcase one to many relationship with reservation.rb properly

--- lib/booking_manager.rb
+++ lib/booking_manager.rb
@@ -5,16 +5,16 @@ module HotelSystem
   class BookingManager
     attr_reader :all_rooms
     
-    def initialize(
-      all_rooms: {1 => [], 2 => [], 3 => [], 4 => [], 5 => [], 6 => [], 7 => [], 8 => [], 9 => [], 10 => [], 11 => [], 12 => [], 13 => [], 14 => [], 15 => [], 16 => [], 17 => [], 18 => [], 19 => [], 20 => []}
-    )
-
-    @all_rooms = all_rooms
+    def initialize
+      @all_rooms = [{1 => [], 2 => [], 3 => [], 4 => [], 5 => [], 6 => [], 7 => [], 8 => [], 9 => [], 10 => [], 11 => [], 12 => [], 13 => [], 14 => [], 15 => [], 16 => [], 17 => [], 18 => [], 19 => [], 20 => []}]
     end
   
     def display_rooms
-      return all_rooms.keys
+      all_rooms[0].keys
     end
 
   end
 end
+
+booking = HotelSystem::BookingManager.new
+p booking.display_rooms
