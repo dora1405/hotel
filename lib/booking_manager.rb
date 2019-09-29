6a81364390664615c255850f38eb43b34617082b
dora1405 <li.dora05@gmail.com>
2019-09-28 18:11:31 -0700

    Revert "update test_helper.rb"
    This reverts commit 24f85e206d3516cd584d741266028a036e29d022.

--- lib/booking_manager.rb
+++ lib/booking_manager.rb
@@ -6,12 +6,15 @@ module HotelSystem
     attr_reader :all_rooms
     
     def initialize
-      @all_rooms = {1 => [], 2 => [], 3 => [], 4 => [], 5 => [], 6 => [], 7 => [], 8 => [], 9 => [], 10 => [], 11 => [], 12 => [], 13 => [], 14 => [], 15 => [], 16 => [], 17 => [], 18 => [], 19 => [], 20 => []}
+      @all_rooms = [{1 => [], 2 => [], 3 => [], 4 => [], 5 => [], 6 => [], 7 => [], 8 => [], 9 => [], 10 => [], 11 => [], 12 => [], 13 => [], 14 => [], 15 => [], 16 => [], 17 => [], 18 => [], 19 => [], 20 => []}]
     end
   
     def display_rooms
-      all_rooms.keys
+      all_rooms[0].keys
     end
 
   end
 end
+
+booking = HotelSystem::BookingManager.new
+p booking.display_rooms
